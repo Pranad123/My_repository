40504ab7377cd92a42df05357aa1c2cdad5999ef
Pranad <129146725+Pranad123@users.noreply.github.com>
2023-05-29 11:59:28 +0530

    Add files via upload

--- photo.py
+++ photo.py
@@ -1,40 +1,62 @@
 import cv2
+from time import sleep
 import mediapipe as mp
 
-def main():
-    # Initialize VideoCapture object
-    cap = cv2.VideoCapture(0)
+# Define a dictionary of sign language gestures and their meanings
 
-    # Initialize Mediapipe Hand object
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    
+    # Initialize MediaPipe Hands
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands()
+    mp_drawing = mp.solutions.drawing_utils
+    with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+        i = 1
+        while cap.isOpened():
+                # Add more gesture recognition logic here
+        
 
-    while True:
-        # Read frame from camera
-        ret, frame = cap.read()
+            success, frame = cap.read()
+            if not success:
+                break
 
-        # Convert image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            # Convert the image to RGB
+            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            cv2.flip(frame ,1)
+            
+            # Process the image with MediaPipe Hands
+            results = hands.process(image_rgb)
+            
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # Draw the landmarks on the image
+                # Draw hand landmarks
+                    index_finger = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                    middle_finger = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                    index_to_middle_dist = abs(index_finger.x - middle_finger.x) + abs(index_finger.y - middle_finger.y) + abs(index_finger.z - middle_finger.z)
 
-        # Process the image with Mediapipe
-        results = hands.process(image)
+                    if index_to_middle_dist > 0.15:
+                        for counter in range(5,0, -1):
+                            cv2.putText(frame,str(counter), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                            cv2.imshow('Sign Recognition', frame)
+                            cv2.waitKey(1000)
+                        print("image saved succsessfully")              
+                        i+=1
 
-        # Check for hand landmarks
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                # Draw hand landmarks on the frame
-                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            # Display the resulting image
+            cv2.imshow('Sign Recognition', frame)
+            
+            if cv2.waitKey(1) == ord('q'):
+                break
 
-        # Display the resulting frame
-        cv2.imshow('Hand Tracking', frame)
+        
+        cap.release()
+        cv2.destroyAllWindows()
 
-        # Check for 'q' key press to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+main()
 
-    # Release the VideoCapture object and close windows
-    cap.release()
-    cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    main()
